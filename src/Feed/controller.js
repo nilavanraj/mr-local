d4af0f1358815c13f386297796e55cb86e6e3e1a
Nilavan Raj <58332892+nilavanraj@users.noreply.github.com>
2022-07-19 10:00:57 +0530

    sub added

--- src/Feed/controller.js
+++ src/Feed/controller.js
@@ -1,10 +1,21 @@
+
 const createFeed = (req,res,__dirname) =>{
 // console.log(req.body)
-
 res.send([{
     title:"What-Bill-Gates-is-afraid-of",
     video:`/videos/What-Bill-Gates-is-afraid-of.mkv`,
-    sub:`/sub/What_Bill_Gates_is_afraid_of.vtt`
+    sub:[{
+        lang:"English",
+        path:"/sub/English.srt"
+    },
+    {
+        lang:"German",
+        path:"/sub/German.srt"
+    },
+    {
+        lang:"Spanish",
+        path:"/sub/Spanish.srt"
+    }]
 }]) 
 }
 export default createFeed;
