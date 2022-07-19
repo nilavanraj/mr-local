67f7237218b91d1f1ed8710296f3fba916d87adc
Nilavan Raj <58332892+nilavanraj@users.noreply.github.com>
2022-07-19 08:24:45 +0530

    setup done

---
+++ index.js
@@ -0,0 +1,56 @@
+import express from "express";
+import createFeed from "./src/Feed/controller.js";
+import bodyParser from "body-parser";
+import mysql from "mysql";
+import 'dotenv/config'
+
+const jsonParser = bodyParser.json()
+const app = express()
+console.log(process.env.DB_USER)
+
+// app.post('/create-feed',jsonParser, function (req, res) { 
+//   createFeed(req, res)
+//  })
+
+//  app.get('/createDB',jsonParser, function (req, res) { 
+//   var con = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     // password: process.env.DB_PASSWORD
+//   });
+//   con.connect(function(err) {
+//     if (err) throw err;
+//     con.query("CREATE DATABASE feed", function (err, result) {
+//       if (err) throw err;
+//       console.log("Database created");
+//     });
+//   });
+//  })
+
+//  app.get('/createTableFeed',jsonParser, function (req, res) { 
+//   var con = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database:process.env.DB,
+//     path : '/quote/random', // the rest of the url with parameters if needed
+//     method : 'GET'
+//   });
+//   con.connect(function(err) {
+//     if (err) throw err;
+//     con.query("CREATE DATABASE feed", function (err, result) {
+//       if (err) throw err;
+//       console.log("Database created");
+//     });
+//   });
+//  })
+
+app.post('/create-feed',jsonParser, function (req, res) { 
+  createFeed(req, res)
+ })
+
+
+
+
+
+app.listen(3000)
---
+++ src/Feed/controller.js
@@ -0,0 +1,5 @@
+const createFeed = (req,res) =>{
+console.log(req.body)
+res.send(req.body) 
+}
+export default createFeed;
