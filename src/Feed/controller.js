389f8de81374195098e93e8d062d9482a528d4fe
Nilavan Raj <58332892+nilavanraj@users.noreply.github.com>
2022-07-30 08:59:29 +0530

    test commit

--- src/Feed/controller.js
+++ src/Feed/controller.js
@@ -1,21 +1,21 @@
 
 const createFeed = (req,res,__dirname) =>{
-// console.log(req.body)
-res.send([{
-    title:"What-Bill-Gates-is-afraid-of",
-    video:`/videos/What-Bill-Gates-is-afraid-of.mkv`,
-    sub:[{
-        lang:"English",
-        path:"/sub/English.json"
-    },
-    {
-        lang:"German",
-        path:"/sub/German.json"
-    },
-    {
-        lang:"Spanish",
-        path:"/sub/Spanish.json"
-    }]
-}]) 
-}
-export default createFeed;
+    // console.log(req.body)
+    res.send([{
+        title:"What-Bill-Gates-is-afraid-of",
+        video:`/videos/What-Bill-Gates-is-afraid-of.mkv`,
+        sub:[{
+            lang:"English",
+            path:"/sub/English.json"
+        },
+        {
+            lang:"German",
+            path:"/sub/German.json"
+        },
+        {
+            lang:"Spanish",
+            path:"/sub/Spanish.json"
+        }]
+    }]) 
+    }
+    export default createFeed;
